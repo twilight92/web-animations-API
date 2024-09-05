afe6a51cd3670f7c9e3d2f232e862edf2b6fa90f
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-05 19:57:07 +0900

    scale 적용

--- waapi-group/Star.js
+++ waapi-group/Star.js
@@ -1,11 +1,12 @@
 export default class Star {
-  constructor(number, x, y) {
+  constructor(number, x, y, scale) {
     const element = document.createElement("div");
     element.classList.add("star");
     element.innerHTML = number;
 
     element.style.left = `${x}px`;
     element.style.top = `${y}px`;
+    element.style.transform = `scale(${scale})`;
     document.body.append(element);
   }
 }
