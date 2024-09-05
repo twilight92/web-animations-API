bd3b8d5e9de0d6968d011698bc4c3830de0c9c0f
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-05 19:51:26 +0900

    스타일 지정 방식 변경/프리티어 적용

--- waapi-group/Star.js
+++ waapi-group/Star.js
@@ -1,10 +1,11 @@
 export default class Star {
-    constructor(number, x, y) {
-        const element = document.createElement('div');
-        element.classList.add('star');
-        element.innerHTML = number;
+  constructor(number, x, y) {
+    const element = document.createElement("div");
+    element.classList.add("star");
+    element.innerHTML = number;
 
-        element.style.cssText = `left: ${x}px; top: ${y}px;`;
-        document.body.append(element);
-    }
-};
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px;`;
+    document.body.append(element);
+  }
+}
