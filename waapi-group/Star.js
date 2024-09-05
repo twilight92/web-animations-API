b17c086e9a2e457df82e7b3562bf0de0cd088d49
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-05 20:22:54 +0900

    애니메이션 추가

--- waapi-group/Star.js
+++ waapi-group/Star.js
@@ -1,12 +1,9 @@
 export default class Star {
-  constructor(number, x, y, scale) {
-    const element = document.createElement("div");
-    element.classList.add("star");
-    element.innerHTML = number;
+  constructor(number) {
+    this.element = document.createElement("div");
+    this.element.classList.add("star");
+    this.element.innerHTML = number;
 
-    element.style.left = `${x}px`;
-    element.style.top = `${y}px`;
-    element.style.transform = `scale(${scale})`;
-    document.body.append(element);
+    document.body.append(this.element);
   }
 }
